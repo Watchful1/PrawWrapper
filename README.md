bec9b45abaa8659a68667a98c5d9595d0dfaf997
Watchful1 <watchful@watchful.gr>
2020-06-16 23:22:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A wrapper for PRAW that lets you easily mock calls in unit tests.
+
+Install with: `pipenv install git+https://github.com/Watchful1/PrawWrapper.git#egg=praw-wrapper`
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="praw-wrapper",
+    version="1.0.0",
+    author="Watchful One",
+    author_email="watchful@watchful.gr",
+    description="A wrapper around PRAW for easier unit testing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Watchful1/PrawWrapper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
