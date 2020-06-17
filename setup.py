8d8bc86f2ae89c72ce851dd47c1dd3f013a286df
Watchful1 <watchful@watchful.gr>
2020-06-16 23:33:01 -0700

    Rename

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="praw-wrapper",
-    version="1.0.2",
+    version="1.0.3",
     author="Watchful One",
     author_email="watchful@watchful.gr",
     description="A wrapper around PRAW for easier unit testing",
