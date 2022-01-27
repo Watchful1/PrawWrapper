b55d04614ac1ed8fb984213a7090e5445fd6e19b
Watchful1 <watchful@watchful.gr>
2022-01-26 19:09:47 -0800

    Add yet more new exceptions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="praw-wrapper",
-    version="1.1.37",
+    version="1.1.38",
     author="Watchful One",
     author_email="watchful@watchful.gr",
     description="A wrapper around PRAW for easier unit testing",
