28f0cdeff345be23a38409192fc631d5739217f8
Watchful1 <watchful@watchful.gr>
2024-08-04 15:27:40 -0700

    Bump revs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="praw-wrapper",
-    version="2.1.2",
+    version="2.1.3",
     author="Watchful One",
     author_email="watchful@watchful.gr",
     description="A wrapper around PRAW for easier unit testing",
